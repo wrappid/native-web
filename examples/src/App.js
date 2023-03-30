78911b05d2086a6806c4506c80aa99b7b1ceeb33
sumanta-m <sumanta.m@vixplor.com>
2023-03-30 16:59:36 +0530

    all fixed sc components added

---
+++ examples/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { SCTypography } from "./package/dataDisplay/SCTypography";
+
+function App() {
+  const clickFun = () => {
+    alert("Hello");
+  };
+
+  return (
+    <>
+      <SCTypography>buiiiiiiiiii</SCTypography>
+    </>
+  );
+}
+
+export default App;
