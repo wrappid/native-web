f43d35d1395765b8a9d100b2684e8f375f4a536e
sumanta-m <sumanta.m@vixplor.com>
2023-04-20 19:43:54 +0530

    menu util fix

--- package/helper/menuUtil.js
+++ package/helper/menuUtil.js
@@ -1,73 +1,154 @@
 import React from "react";
-import CoreDivider from "../components/dataDisplay/CoreDivider";
-import CoreIcon from "../components/dataDisplay/CoreIcon";
-import CoreListItemIcon from "../components/dataDisplay/CoreListItemIcon";
-import CoreListItemText from "../components/dataDisplay/CoreListItemText";
-import CoreIconButton from "../components/inputs/CoreIconButton";
-import CoreLink from "../components/navigation/CoreLink";
-import CoreMenuItem from "../components/navigation/CoreMenuItem";
 import { CoreClasses } from "@wrappid/styles";
-import {
-  MENU_HEADER_ITEM,
-  MENU_ITEM,
-  MENU_ITEM_BUTTON,
-  MENU_ITEM_ICON,
-  MENU_ITEM_TEXT,
-  MENU_ITEM_WRAPPER,
-  MENU_PARENT_ITEM,
-  MENU_SEPERATOR,
-} from "../config/menuConstants";
 import { isJson } from "./stringUtils";
-import { getCoreTheme } from "./themeUtil";
+import NativeDivider from "../nativeComponents/dataDisplay/NativeDivider";
+import NativeLink from "../nativeComponents/navigation/NativeLink";
+import NativeMenuItem from "../nativeComponents/navigation/NativeMenuItem";
+import NativeListItemIcon from "../nativeComponents/dataDisplay/NativeListItemIcon";
+import NativeIcon from "../nativeComponents/dataDisplay/NativeIcon";
+import NativeListItemText from "../nativeComponents/dataDisplay/NativeListItemText";
+import NativeIconButton from "../nativeComponents/inputs/NativeIconButton";
 
-export function createMultiLevelMenu(
-  menu,
+export default function getMenuItem(
+  menuItem,
   level,
   OnMenuClick,
   miniDrawer,
-  multiLevel,
   open,
-  openCollapse,
   selectedID,
   setSelectedID,
-  locationPathname
+  locationPathname,
+  theme,
+  allTypes
 ) {
-  return menu?.map((itemData) => {
-    return (
-      <>
-        {getMenuItem(
-          itemData,
-          level,
-          OnMenuClick,
-          miniDrawer,
-          open,
-          selectedID,
-          setSelectedID,
-          locationPathname
-        )}
-        {multiLevel &&
-          itemData?.Children &&
-          itemData?.Children.length > 0 &&
-          openCollapse[itemData.id] &&
-          createMultiLevelMenu(
-            itemData?.Children,
-            level + 1,
-            OnMenuClick,
-            miniDrawer,
-            multiLevel,
-            open,
-            openCollapse,
-            selectedID,
-            setSelectedID,
-            locationPathname
-          )}
-      </>
-    );
-  });
+  // console.log("OPEN", open);
+  /**
+   * @todo review required for using core menu item instead of core List Item
+   */
+  return menuItem.type === MENU_SEPERATOR ? (
+    <NativeDivider />
+  ) : open ? (
+    <NativeLink
+      href={
+        menuItem?.type === MENU_ITEM && menuItem?.link
+          ? menuItem?.link
+          : "javascript:void(0)"
+      }
+    >
+      <NativeMenuItem
+        sx={{
+          height: "34px",
+          paddingLeft: `calc(13px + ${level * 8}px)`,
+          backgroundColor:
+            menuItem?.type === MENU_ITEM &&
+            locationPathname === menuItem?.link &&
+            theme.palette.secondary.light,
+        }}
+        key={menuItem.id}
+        disablePadding
+        title={menuItem.label}
+        onClick={(e) => {
+          // setSelectedID(menuItem?.name);
+          OnMenuClick(menuItem);
+        }}
+      >
+        <NativeListItemIcon
+          styleClasses={
+            miniDrawer
+              ? [
+                  CoreClasses.MENU.MINI_DRAWER_LIST_ITEM_ICON,
+                  ...getTypeWiseStyle(menuItem, MENU_ITEM_ICON, allTypes),
+                ]
+              : [
+                  CoreClasses.MENU.LIST_ITEM_ICON,
+                  ...getTypeWiseStyle(menuItem, MENU_ITEM_ICON, allTypes),
+                ]
+          }
+        >
+          <NativeIcon
+            options={
+              typeof menuItem?.icon === "object"
+                ? menuItem?.icon
+                : typeof menuItem?.icon === "string" && isJson(menuItem?.icon)
+                ? JSON.parse(menuItem?.icon)
+                : { icon: menuItem?.icon }
+            }
+            sx={{
+              color: `${
+                menuItem?.type === MENU_ITEM &&
+                locationPathname === menuItem?.link
+                  ? theme.palette.primary.light
+                  : theme.palette.secondary.dark
+              }!important`,
+            }}
+          />
+        </NativeListItemIcon>
+        <NativeListItemText
+          disableTypography
+          styleClasses={[
+            CoreClasses.NAVIGATION.APP_DRAWER_TEXT,
+            ...getTypeWiseStyle(menuItem, MENU_ITEM_TEXT, allTypes),
+          ]}
+          primary={menuItem.label}
+          sx={{
+            color: `${
+              menuItem?.type === MENU_ITEM &&
+              locationPathname === menuItem?.link
+                ? theme.palette.primary.light
+                : theme.palette.secondary.dark
+            }!important`,
+          }}
+        />
+      </NativeMenuItem>
+    </NativeLink>
+  ) : (
+    <NativeLink
+      href={
+        menuItem?.type === MENU_ITEM && menuItem?.link
+          ? menuItem?.link
+          : "javascript:void(0)"
+      }
+    >
+      <NativeIconButton
+        title={menuItem?.label}
+        titlePlacement={"right"}
+        onClick={(e) => {
+          setSelectedID(menuItem?.name);
+          OnMenuClick(menuItem);
+        }}
+      >
+        <NativeIcon
+          options={
+            isJson(menuItem?.icon)
+              ? JSON.parse(menuItem?.icon)
+              : { icon: menuItem?.icon }
+          }
+          sx={{
+            color: `${
+              menuItem?.type === MENU_ITEM &&
+              locationPathname === menuItem?.link
+                ? theme.palette.primary.light
+                : theme.palette.secondary.dark
+            }!important`,
+          }}
+        ></NativeIcon>
+      </NativeIconButton>
+    </NativeLink>
+  );
 }
 
-function getTypeWiseStyle(item, elemType) {
+function getTypeWiseStyle(item, elemType, allTypes) {
   var styles = [];
+  const {
+    MENU_PARENT_ITEM,
+    MENU_ITEM_WRAPPER,
+    MENU_ITEM_BUTTON,
+    MENU_ITEM_ICON,
+    MENU_ITEM_TEXT,
+    MENU_HEADER_ITEM,
+    MENU_SEPERATOR,
+    MENU_ITEM,
+  } = allTypes;
   switch (item.type) {
     case MENU_PARENT_ITEM:
       if (elemType === MENU_ITEM_WRAPPER) {
@@ -128,131 +209,4 @@ function getTypeWiseStyle(item, elemType) {
       break;
   }
   return styles;
-}
-
-function getMenuItem(
-  menuItem,
-  level,
-  OnMenuClick,
-  miniDrawer,
-  open,
-  selectedID,
-  setSelectedID,
-  locationPathname
-) {
-  // console.log("OPEN", open);
-  /**
-   * @todo review required for using core menu item instead of core List Item
-   */
-  const theme = getCoreTheme();
-  return menuItem.type === MENU_SEPERATOR ? (
-    <CoreDivider />
-  ) : open ? (
-    <CoreLink
-      href={
-        menuItem?.type === MENU_ITEM && menuItem?.link
-          ? menuItem?.link
-          : "javascript:void(0)"
-      }
-    >
-      <CoreMenuItem
-        sx={{
-          height: "34px",
-          paddingLeft: `calc(13px + ${level * 8}px)`,
-          backgroundColor:
-            menuItem?.type === MENU_ITEM &&
-            locationPathname === menuItem?.link &&
-            theme.palette.secondary.light,
-        }}
-        key={menuItem.id}
-        disablePadding
-        title={menuItem.label}
-        onClick={(e) => {
-          // setSelectedID(menuItem?.name);
-          OnMenuClick(menuItem);
-        }}
-      >
-        <CoreListItemIcon
-          styleClasses={
-            miniDrawer
-              ? [
-                  CoreClasses.MENU.MINI_DRAWER_LIST_ITEM_ICON,
-                  ...getTypeWiseStyle(menuItem, MENU_ITEM_ICON),
-                ]
-              : [
-                  CoreClasses.MENU.LIST_ITEM_ICON,
-                  ...getTypeWiseStyle(menuItem, MENU_ITEM_ICON),
-                ]
-          }
-        >
-          <CoreIcon
-            options={
-              typeof menuItem?.icon === "object"
-                ? menuItem?.icon
-                : typeof menuItem?.icon === "string" && isJson(menuItem?.icon)
-                ? JSON.parse(menuItem?.icon)
-                : { icon: menuItem?.icon }
-            }
-            sx={{
-              color: `${
-                menuItem?.type === MENU_ITEM &&
-                locationPathname === menuItem?.link
-                  ? theme.palette.primary.light
-                  : theme.palette.secondary.dark
-              }!important`,
-            }}
-          />
-        </CoreListItemIcon>
-        <CoreListItemText
-          disableTypography
-          styleClasses={[
-            CoreClasses.NAVIGATION.APP_DRAWER_TEXT,
-            ...getTypeWiseStyle(menuItem, MENU_ITEM_TEXT),
-          ]}
-          primary={menuItem.label}
-          sx={{
-            color: `${
-              menuItem?.type === MENU_ITEM &&
-              locationPathname === menuItem?.link
-                ? theme.palette.primary.light
-                : theme.palette.secondary.dark
-            }!important`,
-          }}
-        />
-      </CoreMenuItem>
-    </CoreLink>
-  ) : (
-    <CoreLink
-      href={
-        menuItem?.type === MENU_ITEM && menuItem?.link
-          ? menuItem?.link
-          : "javascript:void(0)"
-      }
-    >
-      <CoreIconButton
-        title={menuItem?.label}
-        titlePlacement={"right"}
-        onClick={(e) => {
-          setSelectedID(menuItem?.name);
-          OnMenuClick(menuItem);
-        }}
-      >
-        <CoreIcon
-          options={
-            isJson(menuItem?.icon)
-              ? JSON.parse(menuItem?.icon)
-              : { icon: menuItem?.icon }
-          }
-          sx={{
-            color: `${
-              menuItem?.type === MENU_ITEM &&
-              locationPathname === menuItem?.link
-                ? theme.palette.primary.light
-                : theme.palette.secondary.dark
-            }!important`,
-          }}
-        ></CoreIcon>
-      </CoreIconButton>
-    </CoreLink>
-  );
 }
