546010302b27c748b04405add3d95011601f220d
sumanta-m <sumanta.m@vixplor.com>
2023-04-21 18:50:00 +0530

    prettier restructuring in native navigation

--- package/nativeComponents/navigation/NativeNavigation.js
+++ package/nativeComponents/navigation/NativeNavigation.js
@@ -1,9 +1,7 @@
-import React from 'react';
+import React from "react";
 import { BrowserRouter } from "react-router-dom";
 
 export default function NativeNavigation(props) {
-    const { children } = props;
-    return (
-        <BrowserRouter>{children}</BrowserRouter>
-    )
+  const { children } = props;
+  return <BrowserRouter>{children}</BrowserRouter>;
 }
