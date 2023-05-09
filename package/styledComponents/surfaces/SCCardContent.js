83ee240015a2a15b7243b82173e9bd86bdd9109f
sumanta-m <sumanta.m@vixplor.com>
2023-05-09 15:43:20 +0530

    bug fix

--- package/styledComponents/surfaces/SCCardContent.js
+++ package/styledComponents/surfaces/SCCardContent.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 import { CardContent } from "@mui/material";
 import { getEffectiveStyle, StyledComponentsClasses } from "@wrappid/styles";
 
-const defaultStyleClasses = [StyledComponentsClasses.SC.SURFACES.CARD_CONTENT];
+const defaultStyleClasses = [StyledComponentsClasses.SURFACES.CARD_CONTENT];
 
 export const SCCardContent = styled(
   CardContent,
