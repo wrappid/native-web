52632be82888fb85be7d7b17abedfbd5de07389a
sumanta-m <sumanta.m@vixplor.com>
2023-05-12 23:03:20 +0530

    native drawer theme correction

--- package/nativeComponents/surfaces/NativeDrawer.js
+++ package/nativeComponents/surfaces/NativeDrawer.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { SCDrawer } from "../../styledComponents/navigation/SCDrawer";
 import { StyledComponentsClasses, getEffectiveStyle } from "@wrappid/styles";
+import { useTheme } from "@mui/material/styles";
 
 export default function NativeDrawer(props) {
+  const materialTheme = useTheme();
   return (
     <SCDrawer
       {...props}
+      theme={materialTheme}
       PaperProps={{
         sx: {
           ...getEffectiveStyle([StyledComponentsClasses.DRAWER.PAPER_HEIGHT]),
