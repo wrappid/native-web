7d5a01e9fdbf4fc6eb8d8e44eef582c6eb056f05
sumanta-m <sumanta.m@vixplor.com>
2023-05-12 22:20:20 +0530

    styleclass issue fix

--- package/nativeComponents/surfaces/NativeAppBar.js
+++ package/nativeComponents/surfaces/NativeAppBar.js
@@ -1,51 +1,12 @@
 import React from "react";
-
-import { useNavigate } from "react-router-dom";
-
-import NativeIcon from "../dataDisplay/NativeIcon";
-import NativeImage from "../dataDisplay/NativeImage";
-import NativeIconButton from "../inputs/NativeIconButton";
-import NativeStack from "../layouts/NativeStack";
-import NativeToolbar from "../surfaces/NativeToolbar";
-import { UtilityClasses, StyledComponentsClasses } from "@wrappid/styles";
 import { SCAppBar } from "../../styledComponents/surfaces/SCAppBar";
 
 export default function NativeAppBar(props) {
-  const navigate = useNavigate();
-  const { handleDrawer, navigateString, rightAppbarMenu, auth } = props;
+  const { auth } = props;
 
   return (
-    <>
-      <SCAppBar auth={auth} position="fixed">
-        <NativeToolbar
-          styleClasses={[
-            UtilityClasses.ALIGNMENT.JUSTIFY_CONTENT_SPACE_BETWEEN,
-          ]}
-        >
-          <NativeStack direction="row">
-            <NativeIconButton
-              aria-label="open drawer"
-              onClick={handleDrawer}
-              edge="start"
-              disabled={!auth?.uid}
-              sx={{ marginLeft: "-16px" }}
-            >
-              <NativeIcon>menu</NativeIcon>
-            </NativeIconButton>
-
-            <NativeImage
-              src="/images/logo.png"
-              alt="Rxefy Logo"
-              onClick={() => {
-                navigate(navigateString);
-              }}
-              styleClasses={[StyledComponentsClasses.CORE_APP_BAR.APP_BAR_LOGO]}
-            />
-          </NativeStack>
-
-          {rightAppbarMenu}
-        </NativeToolbar>
-      </SCAppBar>
-    </>
+    <SCAppBar {...props} position="fixed">
+      {props.children}
+    </SCAppBar>
   );
 }
