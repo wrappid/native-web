1e7593d0f3b202f1b5e789e0d268fba2387ba277
sumanta-m <sumanta.m@vixplor.com>
2023-05-12 19:54:45 +0530

    native icon change

--- package/nativeComponents/dataDisplay/NativeIcon.js
+++ package/nativeComponents/dataDisplay/NativeIcon.js
@@ -1,14 +1,6 @@
 import React from "react";
 import { SCIcon } from "../../styledComponents/dataDisplay/SCIcon";
 
-export const __IconTypes = {
-  MATERIAL_ICON: "MATERIAL_ICON", // Default support of MUI
-  MATERIAL_OUTLINED_ICON: "MATERIAL_OUTLINED_ICON",
-  FONTAWESOME_V5_SOLID_ICON: "FONTAWESOME_V5_SOLID_ICON",
-  FONTAWESOME_V5_REGULAR_ICON: "FONTAWESOME_V5_REGULAR_ICON",
-  RXICON_V1_REGULAR_ICON: "RXICON_V1_REGULAR_ICON",
-};
-
 /**
  * - important notice
  * - children > props > options(local/db) > default
@@ -17,62 +9,15 @@ export const __IconTypes = {
  * @returns
  */
 export default function NativeIcon(props) {
-  const { type, icon, options, sx, ...restProps } = props;
+  const { type, name, styleClasses, sx, size } = props;
 
-  const renderNativeIcon = () => {
-    let tmpType = type || options?.type || __IconTypes.MATERIAL_ICON;
-    let tmpIcon = props.children || icon || options?.icon || "";
-    switch (tmpType) {
-      case __IconTypes.RXICON_V1_REGULAR_ICON:
-        return (
-          <SCIcon
-            sx={{ ...sx, overflow: "unset" }}
-            baseClassName="rxi"
-            className={tmpIcon}
-            size="small"
-            {...restProps}
-          />
-        );
-      case __IconTypes.FONTAWESOME_V5_REGULAR_ICON:
-        return (
-          <SCIcon
-            sx={{ ...sx, overflow: "unset" }}
-            baseClassName="far"
-            className={tmpIcon}
-            size="small"
-            {...restProps}
-          />
-        );
-      case __IconTypes.FONTAWESOME_V5_SOLID_ICON:
-        return (
-          <SCIcon
-            sx={{ ...sx, overflow: "unset" }}
-            baseClassName="fas"
-            className={tmpIcon}
-            size="small"
-            {...restProps}
-          />
-        );
-      case __IconTypes.MATERIAL_OUTLINED_ICON:
-        return (
-          <SCIcon
-            baseClassName="material-icons-outlined"
-            sx={sx}
-            size="small"
-            {...restProps}
-          >
-            {tmpIcon}
-          </SCIcon>
-        );
-      case __IconTypes.MATERIAL_ICON:
-      default:
-        return (
-          <SCIcon sx={sx} size="small" {...restProps}>
-            {tmpIcon}
-          </SCIcon>
-        );
-    }
-  };
-
-  return <>{renderNativeIcon()}</>;
+  return (
+    <SCIcon
+      baseClasssName={type}
+      className={name}
+      size={size || "small"}
+      sx={sx}
+      styleClasses={styleClasses || []}
+    />
+  );
 }
