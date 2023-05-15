b9dff74141aaf20a925ae4995d8da51d56fbea23
sumanta-m <sumanta.m@vixplor.com>
2023-05-15 21:32:10 +0530

    table sort label correction

--- package/nativeComponents/dataDisplay/NativeTableSortLabel.js
+++ package/nativeComponents/dataDisplay/NativeTableSortLabel.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { SCTableSortLabel } from "../../styledComponents/dataDisplay/SCTableRow";
+import { SCTableSortLabel } from "../../styledComponents/dataDisplay/SCTableSortLabel";
 
 export default function NativeTableSortLabel(props) {
   return <SCTableSortLabel {...props}>{props.children}</SCTableSortLabel>;
