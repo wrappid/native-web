ef22d7b965f4bbebf69f0ccaac4247bd402e5ead
sumanta-m <sumanta.m@vixplor.com>
2023-06-20 19:09:17 +0530

    manual tab seection

--- package/nativeComponents/navigation/NativeTab.js
+++ package/nativeComponents/navigation/NativeTab.js
@@ -3,5 +3,24 @@ import React from "react";
 import { SCTab } from "../../styledComponents/navigation/SCTab";
 
 export default function NativeTab(props) {
-  return <SCTab {...props}>{props.children}</SCTab>;
+  return (
+    <SCTab
+      {...props}
+      style={
+        value == currentTab
+          ? {
+              color: theme.palette.primary.main,
+              borderBottom: "solid",
+              borderBottomWidth: 2,
+            }
+          : {}
+      }
+      onClick={(e) => {
+        onClick(e, value);
+      }}
+      selected={value == currentTab}
+    >
+      {props.children}
+    </SCTab>
+  );
 }
