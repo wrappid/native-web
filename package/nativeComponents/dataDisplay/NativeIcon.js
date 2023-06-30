50af56841d42b89e8b7a8a8abb05dcf5140e58c8
Aditya Jaiswal <aditya.jaiswal.cse23@heritageit.edu.in>
2023-06-30 16:33:24 +0530

    + menu icon fixed

--- package/nativeComponents/dataDisplay/NativeIcon.js
+++ package/nativeComponents/dataDisplay/NativeIcon.js
@@ -8,12 +8,21 @@ import { SCIcon } from "../../styledComponents/dataDisplay/SCIcon";
  * @param {*} props
  * @returns
  */
+
+export const __IconTypes = {
+  MATERIAL_ICON: "material-icons", // Default support of MUI
+  MATERIAL_OUTLINED_ICON: "material-icons-outlined",
+  FONTAWESOME_V5_SOLID_ICON: "fas",
+  FONTAWESOME_V5_REGULAR_ICON: "far",
+  RXICON_V1_REGULAR_ICON: "rxi",
+};
+
 export default function NativeIcon(props) {
   const { type, name, styleClasses, sx, size } = props;
 
   return props.childrenFlag ? (
     <SCIcon
-      baseClasssName={type}
+      baseClasssName={Object.keys(__IconTypes).include(type) ? __IconTypes[type] : type}
       className={name}
       size={size || "small"}
       sx={sx}
