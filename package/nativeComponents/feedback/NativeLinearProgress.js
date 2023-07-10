ae98674b3020479d3903fc22bab8773016dbfde7
Aditya Jaiswal <aditya.jaiswal.cse23@heritageit.edu.in>
2023-07-10 13:17:27 +0530

    + minor change

--- package/nativeComponents/feedback/NativeLinearProgress.js
+++ package/nativeComponents/feedback/NativeLinearProgress.js
@@ -2,5 +2,5 @@ import React from "react";
 import { SCLinearProgress } from "../../styledComponents/feedback/SCLinearProgress";
 
 export default function NativeLinearProgress(props) {
-  return <SCLinearProgress {...props}>{props.children}</SCLinearProgress>;
+  return <SCLinearProgress color="success" {...props}>{props.children}</SCLinearProgress>;
 }
