e4d78df1d976e8bf6814e6cb56843c62833c06e0
sumanta-m <sumanta.m@vixplor.com>
2023-07-14 17:59:50 +0530

    native link styles

--- package/nativeComponents/navigation/NativeLink.js
+++ package/nativeComponents/navigation/NativeLink.js
@@ -3,29 +3,25 @@ import React from "react";
 import { SCLink } from "../../styledComponents/navigation/SCLink";
 import NativeTooltip from "../dataDisplay/NativeTooltip";
 import NativeTypographyCaption from "../dataDisplay/paragraph/NativeTypographyCaption";
-import { CoreClasses } from "@wrappid/core";
+import { UtilityClasses } from "@wrappid/styles";
 
 export default function NativeLink(props) {
   const { title, titlePlacement = "top", size = "small", ...restProps } = props;
+  const newTabFlag = restProps?.href?.includes('http')?true: false
+
   return (
     <>
       {title ? (
         <NativeTooltip title={title} arrow placement={titlePlacement}>
-          <SCLink {...restProps} underline="none">
-            <NativeTypographyCaption
-              style={{ fontSize: 13 }}
-              styleClasses={[CoreClasses.TEXT.TEXT_WEIGHT_BOLD]}
-            >
+          <SCLink {...restProps} underline="none" target={newTabFlag?"_blank":""} rel={newTabFlag?"noreferrer":""}>
+            <NativeTypographyCaption styleClasses={[UtilityClasses?.LINK?.MUI]}>
               {restProps.children}
             </NativeTypographyCaption>
           </SCLink>
         </NativeTooltip>
       ) : (
-        <SCLink {...restProps} underline="none">
-          <NativeTypographyCaption
-            style={{ fontSize: 13 }}
-            styleClasses={[CoreClasses.TEXT.TEXT_WEIGHT_BOLD]}
-          >
+        <SCLink {...restProps} underline="none" target={newTabFlag?"_blank":""} rel={newTabFlag?"noreferrer":""}>
+          <NativeTypographyCaption styleClasses={[UtilityClasses?.LINK?.MUI]}>
             {restProps.children}
           </NativeTypographyCaption>
         </SCLink>
