783c7faf5d824cad35cd15a40212e5d8eb6a2876
sumanta-m <sumanta.m@vixplor.com>
2023-07-31 13:52:30 +0530

    auth theme change

--- package/nativeComponents/layouts/NativeAppContainer.js
+++ package/nativeComponents/layouts/NativeAppContainer.js
@@ -2,9 +2,11 @@ import React from "react";
 import NativeBox from "./NativeBox";
 import { UtilityClasses } from "@wrappid/styles";
 import { NativeCssBaseline } from "../../theme/themeUtil";
+import { useTheme } from "@mui/material";
 
 export default function NativeAppContainer(props) {
   const { appBar, leftDrawer, footer, uid, coreClasses } = props;
+  const theme = useTheme()
   return (
     <NativeBox
       styleClasses={[
@@ -17,6 +19,7 @@ export default function NativeAppContainer(props) {
       {leftDrawer()}
       <NativeBox
         component="main"
+        style={uid?{}:{backgroundColor: theme.palette.primary.main}}
         styleClasses={
           uid
           ? [coreClasses.LAYOUT.CONTENT_CONTAINER]
