8df9aed1929635e634b2caaddf823ab6d56835ef
sumanta-m <sumanta.m@vixplor.com>
2023-08-30 23:27:22 +0530

    image picker correction

--- package/nativeComponents/inputs/NativeImagePicker.js
+++ package/nativeComponents/inputs/NativeImagePicker.js
@@ -3,10 +3,23 @@ import { useRef } from "react";
 import { SCInput } from "../../styledComponents/inputs/SCInput";
 import NativeAvatar from "../dataDisplay/NativeAvatar";
 import NativeBadge from "../dataDisplay/NativeBadge";
-import NativeIcon from "../dataDisplay/NativeIcon";
+import NativeIcon, { __IconTypes } from "../dataDisplay/NativeIcon";
 import NativeBox from "../layouts/NativeBox";
 
 export default function NativeImagePicker(props) {
+  const {
+    id,
+    formik,
+    label,
+    value,
+    defaultImage,
+    placeholder,
+    disabled,
+    readOnly,
+    touched,
+    error,
+    ...restProps
+  } = props;
   const inputRef = useRef(null);
   const handleFileOpen = () => {
     console.log("adasd");
@@ -15,7 +28,7 @@ export default function NativeImagePicker(props) {
   return (
     <>
       <SCInput
-        id={props.id}
+        id={id}
         inputRef={inputRef}
         type="file"
         sx={{ display: "none" }}
@@ -23,32 +36,31 @@ export default function NativeImagePicker(props) {
           readOnly: true,
           accept: "image/*",
         }}
-        label={props.label}
+        label={label}
         variant="standard"
-        // value={props.value}
         onChange={(event) => {
-          console.log("FIle Change", event.target.files[0], props.formik);
-          props.formik.setFieldValue(props.id, event.target.files[0]);
+          console.log("FIle Change", event.target.files[0], formik);
+          formik.setFieldValue(id, event.target.files[0]);
         }}
-        required={props.formik ? false : props.required}
-        placeholder={props.placeholder}
-        disabled={props.disabled}
-        max={props.max}
-        min={props.min}
-        readOnly={props.readOnly}
+        required={formik ? false : required}
+        placeholder={placeholder}
+        disabled={disabled}
+        readOnly={readOnly}
         error={
-          props.touched && props.error && props.error.length > 0 ? true : false
+          touched && error && error.length > 0 ? true : false
         }
       />
-      {/* {!props.readOnly && ( */}
+      {/* {!readOnly && ( */}
 
       {/* )} */}
-      {!props.readOnly ? (
+      {!readOnly ? (
         <NativeBox
-          styleClasses={[
-            // CoreClasses.NAVIGATION.LINK_STYLE,
-            // CoreClasses.ALIGNMENT.JUSTIFY_CONTENT_CENTER,
-          ]}
+          styleClasses={
+            [
+              // CoreClasses.NAVIGATION.LINK_STYLE,
+              // CoreClasses.ALIGNMENT.JUSTIFY_CONTENT_CENTER,
+            ]
+          }
           aria-label="toggle password visibility"
           type="file"
           onClick={handleFileOpen}
@@ -58,17 +70,19 @@ export default function NativeImagePicker(props) {
           <NativeBadge
             overlap="circular"
             anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
-            badgeContent={<NativeIcon>photo_camera</NativeIcon>}
+            badgeContent={
+              <NativeIcon childrenFlag={true} name="photo_camera" type={__IconTypes.MATERIAL_ICON} />
+            }
           >
             <NativeAvatar
               type="file"
-              styleClasses={props.styleClasses}
+              styleClasses={styleClasses}
               src={
-                props.value
-                  ? typeof props.value === "string"
-                    ? props.value
-                    : URL.createObjectURL(props.value)
-                  : "no_image.png"
+                value
+                  ? typeof value === "string"
+                    ? value
+                    : URL.createObjectURL(value)
+                  : defaultImage
               }
             />
           </NativeBadge>
@@ -76,12 +90,12 @@ export default function NativeImagePicker(props) {
       ) : (
         <NativeAvatar
           type="file"
-          styleClasses={props.styleClasses}
+          styleClasses={styleClasses}
           src={
-            props.value
-              ? typeof props.value === "string"
-                ? props.value
-                : URL.createObjectURL(props.value)
+            value
+              ? typeof value === "string"
+                ? value
+                : URL.createObjectURL(value)
               : "no_image.png"
           }
         />
