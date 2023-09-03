8b2f0c989c2299b0290a913870adc381f753888e
techoneel <contact.techoneel@gmail.com>
2023-09-03 14:50:40 +0530

    + avatar text support provided

--- package/nativeComponents/dataDisplay/NativeAvatar.js
+++ package/nativeComponents/dataDisplay/NativeAvatar.js
@@ -2,5 +2,6 @@ import React from "react";
 import { SCAvatar } from "../../styledComponents/dataDisplay/SCAvatar";
 
 export default function NativeAvatar(props) {
-  return <SCAvatar {...props} />;
+  const { src, children, ...restProps } = props;
+  return children ? (<SCAvatar {...restProps}>{children}</SCAvatar>) : ( <SCAvatar src={src} {...restProps} /> );
 }
