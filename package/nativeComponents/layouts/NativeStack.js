8013079085aba6a5eb33938eca0e1e1e75e138b7
sumanta-m <sumanta.m@vixplor.com>
2023-09-16 00:27:47 +0530

    bug fix

--- package/nativeComponents/layouts/NativeStack.js
+++ package/nativeComponents/layouts/NativeStack.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 import { SCStack } from "../../styledComponents/layouts/SCStack";
+import NativeDivider from "../dataDisplay/NativeDivider";
 
 export default function NativeStack(props) {
-  return <SCStack {...props}>{props.children}</SCStack>;
+  const {divider, direction, children, ...restProps} = props;
+
+  let dividerComp = null
+  if(typeof divider === "boolean"){
+    dividerComp = <NativeDivider orientation={direction === "row"?"vertical":"horizontal"} flexItem/>
+  }
+  else{
+    dividerComp = divider
+  }
+
+  return <SCStack {...restProps} divider={dividerComp} direction={direction}>{children}</SCStack>;
 }
