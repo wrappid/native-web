92d46a690381dacfc18ab6a7119ed214d746b1e5
Nilotpal Saha <nilotpal.saha00@gmail.com>
2024-03-17 00:50:08 +0530

    fix(core): :zap: replace getConfigurationObject with WrappidData
    enhancement: removing getConfigurationObject and replace it with WrappidData
    
    Ref #51

--- package/styledComponents/navigation/SCRightDrawer.js
+++ package/styledComponents/navigation/SCRightDrawer.js
@@ -1,13 +1,12 @@
 // eslint-disable-next-line no-unused-vars, unused-imports/no-unused-imports
-import React from "react";
 
 import MuiDrawer from "@mui/material/Drawer";
 // eslint-disable-next-line import/no-unresolved
-import { getEffectiveStyle, StyledComponentsClasses, getConfigurationObject } from "@wrappid/styles";
+import { StyledComponentsClasses, WrappidData, getEffectiveStyle } from "@wrappid/styles";
 import styled from "styled-components";
 
 import { DEFAULT_DRAWER_WIDTH } from "./SCDrawer";
-const config = getConfigurationObject()?.wrappid;
+const { config } = WrappidData;
 
 export const SCRightDrawer = styled(
   MuiDrawer,
