679514ca98f56cec53759958a40722b754f852f8
Nilotpal Saha <nilotpal.saha00@gmail.com>
2024-04-03 13:59:37 +0530

    fix(core): :zap: native card clickable func added
    native card clickable func added
    
    Ref #55

--- package/nativeComponents/surfaces/NativeCard.js
+++ package/nativeComponents/surfaces/NativeCard.js
@@ -1,8 +1,16 @@
 // eslint-disable-next-line no-unused-vars, unused-imports/no-unused-imports
 import React from "react";
 
+import { ButtonBase } from "@mui/material";
+
 import { SCCard } from "../../styledComponents/surfaces/SCCard";
 
 export default function NativeCard(props) {
-  return <SCCard {...props}>{props.children}</SCCard>;
+  return props?.onClick ? (
+    <SCCard {...props}>
+      <ButtonBase onClick={props?.onClick}>{props.children}</ButtonBase>
+    </SCCard>
+  ) : (
+    <SCCard {...props }> { props.children }</SCCard >
+  );
 }
