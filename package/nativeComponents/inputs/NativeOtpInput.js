c135513497de048fca096e878b058c8db411a104
Pritam Bag <130136389+PritamBag@users.noreply.github.com>
2024-07-20 13:56:07 +0530

    fix(core): :bug: update code in NativeOtpInput to fix the issue
    Deleting a character directly without selecting it first. Pasting the OTP using Ctrl+V
    
    ref: #82

--- package/nativeComponents/inputs/NativeOtpInput.js
+++ package/nativeComponents/inputs/NativeOtpInput.js
@@ -1,13 +1,67 @@
 // eslint-disable-next-line import/order, no-unused-vars, unused-imports/no-unused-imports
-import React from "react";
+import React, { useState, useRef } from "react";
 import { useTheme } from "@mui/material";
 import OtpInput from "react-otp-input";
 
 export default function NativeOtpInput(props) {
   const theme = useTheme();
+  const [otp, setOtp] = useState("");
+  const inputRefs = useRef([]);
+
+  // Handle OTP change
+  const handleChange = (val) => {
+    setOtp(val);
+    const length = val.length;
+
+    if (inputRefs.current[length]) {
+      inputRefs.current[length].focus();
+    }
+  };
+
+  // Handle keydown for backspace 
+  const handleKeyDown = (event, index) => {
+    if (event.key === "Backspace") {
+      event.preventDefault(); // Prevent default backspace behavior
+      if (index > 0 && !otp[index]) {
+        setOtp((prevOtp) => prevOtp.slice(0, index - 1) + prevOtp.slice(index));
+        inputRefs.current[index - 1].focus();
+      } else {
+        setOtp((prevOtp) => prevOtp.slice(0, index) + prevOtp.slice(index + 1));
+      }
+    }
+  };
+
+  // Handle paste event
+  const handlePaste = (event) => {
+    event.preventDefault();
+    const paste = event.clipboardData.getData("text").slice(0, 6);
+
+    setOtp(paste);
+
+    // Focus the correct input field after paste
+    setTimeout(() => {
+      const pasteLength = paste.length;
+
+      if (inputRefs.current[pasteLength]) {
+        inputRefs.current[pasteLength].focus();
+        inputRefs.current[pasteLength].setSelectionRange(
+          pasteLength,
+          pasteLength
+        ); // Adjust selection range
+      } else if (inputRefs.current[5]) {
+        inputRefs.current[5].focus();
+        inputRefs.current[5].setSelectionRange(6, 6); // Adjust selection range
+      }
+    }, 0);
+  };
+
+  React.useEffect(() => {
+    props.formik.setFieldValue(props.id, otp);
+  }, [otp]);
 
   return (
     <OtpInput
+      onChange={handleChange}
       containerStyle={{
         // width: "100%"
         display    : "flex",
@@ -25,16 +79,23 @@ export default function NativeOtpInput(props) {
         fontSize         : "2rem",
         margin           : 10,
         outline          : "none",
-        width            : "100%"
+        width            : "100%",
       }}
       id={props.id}
       name={props.id}
-      value={props.value}
-      onChange={(val) => {
-        props.formik.setFieldValue(props.id, val);
-      }}
+      value={otp}
+      // onChange={(val) => {
+      //   props.formik.setFieldValue(props.id, val);
+      // }}
       numInputs={6}
-      renderInput={(props) => <input {...props} />}
+      renderInput={(props, index) => (
+        <input
+          {...props}
+          ref={(element) => (inputRefs.current[index] = element)}
+          onKeyDown={(event) => handleKeyDown(event, index)}
+          onPaste={handlePaste} // paste event handler
+        />
+      )}
       renderSeparator={<span></span>}
       shouldAutoFocus={props.autoFocus == false ? false : true}
       inputType="tel"
