12f943e9a5d5b7a9dcf29828593501b2d646a63b
arkaprota <arkaprota.ghosh@gmail.com>
2024-08-19 19:03:51 +0530

    fix(core): :bug: corelistitemsecondaryaction has been implemented
    corelistitemsecondaryaction has been implemented successfully
    
    Ref: #101

---
+++ package/nativeComponents/dataDisplay/NativeListItemSecondaryAction.js
@@ -0,0 +1,8 @@
+// eslint-disable-next-line no-unused-vars, unused-imports/no-unused-imports
+import React from "react";
+
+import { SCListItemSecondaryAction } from "../../styledComponents/dataDisplay/SCListItemSecondaryAction";
+
+export default function NativeListItemSecondaryAction(props) {
+  return <SCListItemSecondaryAction {...props} />;
+}
