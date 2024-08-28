b116a9f0bddfe9b61456942ddf5a82168f7a31ec
Pritam Bag <130136389+PritamBag@users.noreply.github.com>
2024-08-28 12:42:32 +0530

    feat(global): :sparkles: passing prop for label
    passing props in LinearProgress, if user pass this prop then label is displayed in UI
    
    Ref: #114

--- package/nativeComponents/feedback/NativeLinearProgress.js
+++ package/nativeComponents/feedback/NativeLinearProgress.js
@@ -1,8 +1,32 @@
 // eslint-disable-next-line no-unused-vars, unused-imports/no-unused-imports
 import React from "react";
 
+// eslint-disable-next-line import/no-unresolved
+import { UtilityClasses } from "@wrappid/styles";
+
 import { SCLinearProgress } from "../../styledComponents/feedback/SCLinearProgress";
+import NativeTypography from "../dataDisplay/NativeTypography";
+import NativeBox from "../layouts/NativeBox";
 
 export default function NativeLinearProgress(props) {
-  return <SCLinearProgress {...props}>{props.children}</SCLinearProgress>;
+  const { children, showPercentage, ...rest } = props;
+  const boxWidth = "35px";
+
+  return (
+    <NativeBox styleClasses={[UtilityClasses.DISPLAY.FLEX, UtilityClasses.ALIGNMENT.ALIGN_ITEMS_CENTER]}>
+      <NativeBox styleClasses={[UtilityClasses.WIDTH.W_100]}>
+        <SCLinearProgress {...rest}>{children}</SCLinearProgress>
+      </NativeBox>
+      
+      {showPercentage && (
+        <NativeBox width={boxWidth} styleClasses={[UtilityClasses.MARGIN.ML1]}>
+          <NativeTypography variant="body1">
+            {`${Math.round(rest.value)}%`}
+          </NativeTypography>
+        </NativeBox>
+      )}
+      
+    </NativeBox>
+
+  );
 }
