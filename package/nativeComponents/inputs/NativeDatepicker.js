41e4e1c727ae44b176ba94213dc55a08b147c252
PritamIT2023 <pritammondal8981@gmail.com>
2024-09-04 14:10:15 +0530

    fix(global): :recycle: formik add in onChange function
    formik add in onChange function
    
    Ref: #116

--- package/nativeComponents/inputs/NativeDatepicker.js
+++ package/nativeComponents/inputs/NativeDatepicker.js
@@ -9,23 +9,30 @@ import { getValidDateTime } from "../../helper/helper";
 import { SCDatePicker } from "../../styledComponents/inputs/SCDatePicker";
 
 export default function NativeDatePicker(props) {
-
-  const [inputText, setInputText] = React.useState(props?.value || "");
-  const onChange = (value) => {
-    props?.onChange && props.onChange(value);
-    setInputText(value);
-  };
   const {
     touched,
     error,
     minDate,
     maxDate,
+    formik,
+    id
   } = props;
 
+  const [date, setDate] = React.useState(props?.value || "");
+  const onChange = (value) => {
+    if(formik){
+      formik?.setFieldValue(id, value);
+    }
+    if(props?.onChange){
+      props?.onChange(value);
+    }
+    setDate(value);
+  };
+ 
   return (
     <LocalizationProvider dateAdapter={AdapterMoment}>
       <SCDatePicker
-        value={props?.value ? String(props.value) : inputText}
+        value={props?.value ? String(props.value) : date}
         minDate={typeof minDate === "string" ? getValidDateTime(minDate) : minDate }
         maxDate={typeof maxDate === "string" ? getValidDateTime(maxDate) : maxDate }
         onChange={onChange}
