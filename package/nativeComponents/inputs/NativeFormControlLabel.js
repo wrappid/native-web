301f2a56b0a39e4d012367ca3d798fc7e86e535a
anantakumarghosh <antaghosh@gmail.com>
2024-09-05 13:42:29 +0530

    refactor(core): :art: spread props
    spread props
    
    ref: #124

--- package/nativeComponents/inputs/NativeFormControlLabel.js
+++ package/nativeComponents/inputs/NativeFormControlLabel.js
@@ -4,5 +4,5 @@ import React from "react";
 import { SCFormControlLabel } from "../../styledComponents/inputs/SCFormControlLabel";
 
 export default function NativeFormControlLabel(props) {
-  return <SCFormControlLabel {...props}>{props.children}</SCFormControlLabel>;
+  return <SCFormControlLabel {...props} />;
 }
