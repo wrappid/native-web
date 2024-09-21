81fe3921e1f5fc5df862c7cf5544e741a99f6809
arkaprota <arkaprota.ghosh@gmail.com>
2024-09-21 20:01:59 +0530

    feat(global): :fire: code structure has been improved in corelist
    the code structure has been improved in corelist
    
    Ref: #136

--- package/nativeComponents/layouts/NativeList.js
+++ package/nativeComponents/layouts/NativeList.js
@@ -6,20 +6,16 @@ import { UtilityClasses } from "@wrappid/styles";
 import NativeGrid from "./NativeGrid";
 import { SCList } from "../../styledComponents/layouts/SCList";
 
-const gridItemComponentObj = {
-  default: 12, l: 4, m: 3, s: 2, xl: 6, xs: 1 
-};
+const defaultItemGridProps = { gridSize: { default: 6, md: 3, sm: 4 }, styleClasses: [UtilityClasses.BORDER.BORDER] };
 
 export default function NativeList(props) {
 
-  const { children, ...restProps } = props;
-  const [gridProps, setGridProps] = React.useState({});
-
-  React.useEffect(() => { setGridProps({ gridSize: { md: gridItemComponentObj[props.gridItemComponent] } }); }, []);
+  const { children, itemGridProps, variant, ...restProps } = props;
+  
   const listStyleClasses = () => {
     let styleClasses = [];
       
-    if (props.variant == "HTML") {
+    if (variant == "HTML") {
       if (props.listType) {
         styleClasses.push(UtilityClasses.PADDING.PL5, UtilityClasses.LIST_STYLE[props.listType]);
       }
@@ -33,61 +29,19 @@ export default function NativeList(props) {
     if (props.listType) {
       styleClasses.push(UtilityClasses.DISPLAY.LIST_ITEM, UtilityClasses.PADDING.P0);
     }
-    if (props.borderList === true ) {
-      styleClasses.push(UtilityClasses.BORDER.BORDER);
-    }
     return [...styleClasses, ...(childProps?.styleClasses || [])];
   };
-
-  if (props.variant == "GRID") {
-    if (props.gridItemComponent) {
-      return <SCList
-        styleClasses={listStyleClasses()}
-        {...restProps}>
-        <NativeGrid>{
-          React.Children.map(children, child => {
-            if (React.isValidElement(child)) {
-              const updatedProps = {
-                ...child.props,
-                gridProps   : gridProps,
-                styleClasses: listItemStyleClasses(child.props)
-              };
-        
-              return React.cloneElement(child, updatedProps); // Pass both props
-            } else {
-              return child;
-            }
-          })}</NativeGrid>
-      </SCList>;
-    } else {
-      return <SCList
-        styleClasses={listStyleClasses()}
-        {...restProps}>
-        <NativeGrid>{
-          React.Children.map(children, child => {
-            if (React.isValidElement(child)) {
-              const updatedProps = {
-                ...child.props,
-                styleClasses: listItemStyleClasses(child.props)
-              };
-        
-              return React.cloneElement(child, updatedProps); // Pass both props
-            } else {
-              return child;
-            }
-          })}</NativeGrid>
-      </SCList>;
-    }
-  }
-  else {
   
-    return <SCList
-      styleClasses={listStyleClasses()}
-      {...restProps}>{
+  return <SCList
+    styleClasses={listStyleClasses()}
+    {...restProps}>
+    {variant == "GRID" ? (
+      <NativeGrid>{
         React.Children.map(children, child => {
           if (React.isValidElement(child)) {
             const updatedProps = {
-              ...child.props, // Spread existing props
+              ...child.props,
+              gridProps   : itemGridProps || defaultItemGridProps,
               styleClasses: listItemStyleClasses(child.props)
             };
         
@@ -95,7 +49,19 @@ export default function NativeList(props) {
           } else {
             return child;
           }
-        })}</SCList>;
-  }
+        })}</NativeGrid>) :
+      (React.Children.map(children, child => {
+        if (React.isValidElement(child)) {
+          const updatedProps = {
+            ...child.props, // Spread existing props
+            styleClasses: listItemStyleClasses(child.props)
+          };
+            
+          return React.cloneElement(child, updatedProps); // Pass both props
+        } else {
+          return child;
+        }
+      }))}    
+  </SCList>;
 }
   
