544ab7f310f7d2322decf5bcdd5c243b0b912536
Mihir Raj <rajmihir945@gmail.com>
2024-09-30 19:43:00 +0530

    feat(core): :sparkles: native component code for login with linkedin
    native component code for login with linkedin
    
    ref: #141

--- package/index.js
+++ package/index.js
@@ -69,6 +69,7 @@ import {
 } from "./nativeComponents/helper/routerHelper";
 import { nativeStorage } from "./nativeComponents/helper/storeHelper";
 import NativeGoogleButton from "./nativeComponents/inputs/custom/NativeGoogleButton";
+import NativeLinkedInAuthComponent from "./nativeComponents/inputs/custom/NativeLinkedInAuthComponent.js";
 import NativeMedicineSelector from "./nativeComponents/inputs/custom/NativeMedicineSelector";
 import NativeReactSelectInput from "./nativeComponents/inputs/custom/NativeReactSelectInput";
 import NativeRichTextEditor from "./nativeComponents/inputs/custom/NativeRichTextEditor.js";
@@ -176,6 +177,6 @@ export {
   NativeTabHead, NativeTable, NativeTableBody, NativeTableBodyCell, NativeTableCell, NativeTableContainer, NativeTableFooter, NativeTableHead, NativeTableHeadCell,
   NativeTableHeadRow, NativeTablePagination, NativeTableRow, NativeTableSortLabel, NativeTabPanel, NativeTabs, NativeTextarea, NativeTextButton, NativeTextField, NativeThemeProvider, NativeTimePicker, NativeTimeRangePicker, NativeToolbar, NativeTooltip, NativeTypography, NativeTypographyBody1, NativeTypographyBody2, NativeTypographyButton, NativeTypographyCaption, NativeTypographyOverline, NativeTypographySubtitle1, NativeTypographySubtitle2, nativeUseLocation, nativeUseNavigate, nativeUseParams, nativeUseScrollTrigger, nativeUseSearchParams, NativeClickAwayListner,
   // Network status custom hook
-  useNetworkStatus, NativeFormControlLabel, NativeFormGroup
+  useNetworkStatus, NativeFormControlLabel, NativeFormGroup,
+  NativeLinkedInAuthComponent
 };
-
