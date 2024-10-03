fbfbdbeaa65a3cb78788d99fce37775824ab9627
PritamIT2023 <pritammondal8981@gmail.com>
2024-10-03 17:03:29 +0530

    refactor(global): :recycle: fix code style of NativeFacebookAuthComponent
    fix code style of NativeFacebookAuthComponent
    
    Ref: #142

--- package/nativeComponents/inputs/custom/NativeFacebookAuthComponent.js
+++ package/nativeComponents/inputs/custom/NativeFacebookAuthComponent.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import NativeButton from '../NativeButton'
+// eslint-disable-next-line no-unused-vars, unused-imports/no-unused-imports
+import React from "react";
+
+import NativeButton from "../NativeButton";
 
 const NativeFacebookAuthComponent = (props) => {
-  
   return (
-    <NativeButton  {...props}/>
+    <NativeButton {...props}/>
+  );
+};
 
-  )
-}
-
-export default NativeFacebookAuthComponent
+export default NativeFacebookAuthComponent;
