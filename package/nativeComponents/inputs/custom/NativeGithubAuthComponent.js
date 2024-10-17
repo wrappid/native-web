0acc1a20c77ca01f2524353e7b72e0b6965d070c
PritamIT2023 <pritammondal8981@gmail.com>
2024-10-17 22:32:03 +0530

    feat(core): :lipstick: socila login icon change
    socila login icon change
    
    Ref: #152

--- package/nativeComponents/inputs/custom/NativeGithubAuthComponent.js
+++ package/nativeComponents/inputs/custom/NativeGithubAuthComponent.js
@@ -2,9 +2,19 @@
 // eslint-disable-next-line unused-imports/no-unused-imports
 import React from "react";
 
-import NativeButton from "../NativeButton";
+// eslint-disable-next-line import/no-unresolved
+import { UtilityClasses } from "@wrappid/styles";
+
+import NativeImage from "../../dataDisplay/NativeImage";
 
 export default function NativeGithubAuthComponent(props) {
     
-  return (<NativeButton {...props}/>);
+  return (
+    <NativeImage
+      styleClasses={[UtilityClasses.CURSOR.CURSOR_POINTER]}
+      src="https://www.svgrepo.com/show/512317/github-142.svg"
+      width="38"
+      height="38"
+      {...props}/>
+  );
 }
